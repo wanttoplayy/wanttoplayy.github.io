e8aa3a1f70f72305ff91ba7dbd15c4b2aa0dbc9d
Vasin <bestwasin1617@gmail.com>
2022-09-29 19:03:35 +0700

    fix:again

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -11,10 +11,10 @@ const NavBar = () => {
 
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-            <div>
-                {/* เดี๋ยวบรรทัดนี้ต้องมาเปลี่ยนLogo */}
+            {/* <div>
+               
                 <img src={Logo} alt="Logo Image" style={{ width: '50px' }} />
-            </div>
+            </div> */}
 
             {/* menu */}
             {/* ตัวเลือนใช้ react-scoll */}
