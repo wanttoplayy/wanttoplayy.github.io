e32afba676f9e507341190264906bdbd89a6d72c
Vasin <bestwasin1617@gmail.com>
2022-09-29 18:32:19 +0700

    fix: no Logo

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Logo from '../asset/Logo.png'
+// import Logo from '../asset/Logo.png'
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
@@ -11,10 +11,10 @@ const NavBar = () => {
 
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-            <div>
-                {/* เดี๋ยวบรรทัดนี้ต้องมาเปลี่ยนLogo */}
+            {/* <div>
+           
                 <img src={Logo} alt="Logo Image" style={{ width: '50px' }} />
-            </div>
+            </div> */}
 
             {/* menu */}
             {/* ตัวเลือนใช้ react-scoll */}
